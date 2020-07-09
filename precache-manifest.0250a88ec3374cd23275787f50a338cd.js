86c940468803af067ddb832de091f209c356dc59
sarahbarron <sarahflavin@yahoo.com>
2020-07-09 01:12:17 +0100

    Updates

---
+++ precache-manifest.0250a88ec3374cd23275787f50a338cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb24b22a834f67515e97e389031cd4c7",
+    "url": "/sarahbarron/react-udemy/index.html"
+  },
+  {
+    "revision": "b931692434e9f94f8b4a",
+    "url": "/sarahbarron/react-udemy/static/css/main.5ba8532b.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/sarahbarron/react-udemy/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sarahbarron/react-udemy/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b931692434e9f94f8b4a",
+    "url": "/sarahbarron/react-udemy/static/js/main.8d20352a.chunk.js"
+  },
+  {
+    "revision": "bace705d9738e180fffd",
+    "url": "/sarahbarron/react-udemy/static/js/runtime-main.aa75f641.js"
+  }
+]);
